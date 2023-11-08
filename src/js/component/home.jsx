9cd963527b168f1acf5d1127245652c2f51c4c55
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2023-11-08 23:49:09 +0000

    Finished Project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,12 +7,24 @@ import rigoImage from "../../img/rigo-baby.jpg";
 function Home() {
 	const [glowing, setGlowing] = useState('red');
 	return (
+		<div>
+			<div className="d-flex justify-content-center">
+				<button type="button" className="btn btn-primary" onClick={() => {
+					if (glowing === "red"){setGlowing("green")}
+					if (glowing === "green"){setGlowing("yellow")}
+					if (glowing === "yellow"){setGlowing("red")}}}>
+					Switch Between Colors
+				</button>
+			</div>
 		<div className="traffic">
-			<div className={"lamp red " + (glowing === 'red' ? "glowing" : "")} onClick={() => setGlowing("red")}></div>
-			<div className={"lamp yellow " + (glowing === 'yellow' ? "glowing" : "")} onClick={() => setGlowing("yellow")}></div>
-			<div className={"lamp green " + (glowing === 'green' ? "glowing" : "")} onClick={() => setGlowing("green")}></div>
+			<div className={"lamp red " + (glowing === 'red' ? "glowingRed" : "")} onClick={() => setGlowing("red")}></div>
+			<div className={"lamp yellow " + (glowing === 'yellow' ? "glowingYellow" : "")} onClick={() => setGlowing("yellow")}></div>
+			<div className={"lamp green " + (glowing === 'green' ? "glowingGreen" : "")} onClick={() => setGlowing("green")}></div>
+			<div className="post"></div>
 		</div>
+</div>
 	);
 };
+
 
 export default Home;
